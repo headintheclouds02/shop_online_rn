5b015b919f1703eac005b27a4b252359b0f21e53
agusansola <agustinaansola@gmail.com>
2026-05-24 18:53:04 +0200

    feat: model for cart item, product and category created

---
+++ types/product.ts
@@ -0,0 +1,14 @@
+export type Category = {
+  id: number;
+  name: string;
+  image: string;
+};
+
+export type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  images: string[];
+  category: Category;
+};
